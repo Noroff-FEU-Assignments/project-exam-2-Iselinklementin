e87440d1e2b708a147879c465a7d1f44e7c30c1f
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-04-21 09:50:53 +0200

    Update Contact.js

--- components/common/forms/Contact.js
+++ components/common/forms/Contact.js
@@ -7,22 +7,10 @@ import { yupResolver } from "@hookform/resolvers/yup";
 const CONTACT_URL = "https://grafs.no/wp-json/wp/v2/holidaze_contact";
 
 const schema = yup.object().shape({
-  name: yup
-    .string()
-    .required("Please enter your name")
-    .min(3, "Your name must at be at least 3 characters"),
-  email: yup
-    .string()
-    .required("Please enter your email address")
-    .email("Please enter a valid email address"),
-  subject: yup
-    .string()
-    .required("Please enter your message")
-    .min(2, "Your subject must at be at least 2 characters"),
-  message: yup
-    .string()
-    .required("Please enter your message")
-    .min(10, "Your message must at be at least 10 characters"),
+  name: yup.string().required("Please enter your name").min(3, "Your name must at be at least 3 characters"),
+  email: yup.string().required("Please enter your email address").email("Please enter a valid email address"),
+  subject: yup.string().required("Please enter your message").min(2, "Your subject must at be at least 2 characters"),
+  message: yup.string().required("Please enter your message").min(10, "Your message must at be at least 10 characters"),
 });
 
 function Contact() {
@@ -56,40 +44,15 @@ function Contact() {
       {submitting}
 
       <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
-        {/* Full Name: */}
-        <input
-          name="your-name"
-          label="Name"
-          type="text"
-          placeholder="Nora Nordmann"
-          {...register("name")}
-        />
+        <input label="Name" type="text" placeholder="Nora Nordmann" {...register("name")} />
 
-        {/* Email: */}
-        <input
-          name="your-email"
-          label="Email"
-          type="text"
-          placeholder="nora@nordmann.no"
-          {...register("email")}
-        />
+        <input label="Email" type="text" placeholder="nora@nordmann.no" {...register("email")} />
 
-        {/* Subject: */}
-        <input
-          name="your-subject"
-          label="Subject"
-          type="text"
-          placeholder="Subject"
-          {...register("subject")}
-        />
+        <input label="Subject" type="text" placeholder="Subject" {...register("subject")} />
 
-        {/* Message: */}
-        <textarea
-          name="your-message"
-          label="Message"
-          placeholder="Message"
-          {...register("message")}
-        />
+        <textarea label="Message" placeholder="Message" {...register("message")} />
+
+        <input name="id" placeholder="id" />
 
         <button type="submit">{submitting ? "sending.." : "send"}</button>
       </form>
