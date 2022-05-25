c2ec58ba0302081205b7b0218331ce89bbf02069
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2022-05-25 21:29:54 +0200

    Update Chips.js

--- components/common/filter/Chips.js
+++ components/common/filter/Chips.js
@@ -1,9 +1,9 @@
-import React, { forwardRef, useState } from "react";
-import Icon, { icons } from "../../../constants/icons";
-import { StyledFilter } from "./StyledFilter.styled";
 import CapitalizeFirstLetter from "../functions/CapitalizeFirstLetter";
 import styled from "styled-components";
 import Heading from "../../../components/typography/Heading";
+import React, { forwardRef } from "react";
+import Icon, { icons } from "../../../constants/icons";
+import { StyledFilter } from "./StyledFilter.styled";
 import { mediaQ } from "../../../styles/global/ThemeConfig";
 
 const StyledHeading = styled(Heading)`
@@ -15,34 +15,11 @@ const StyledHeading = styled(Heading)`
 `;
 
 export const Chips = forwardRef((props, ref) => {
+  const stays = ["bed", "apartment", "hotel"];
+
   const clickedButtonHandler = (e) => {
     e.currentTarget.classList.toggle("active");
   };
-
-  let stayIcons = "";
-
-  /* Nå er ikke ikonene koblet opp, kan fjernes hvis jeg ikke fikser (med funksjonen) */
-  /* {ShowIcon(btnName)} */
-
-  const ShowIcon = (btnName) => {
-    icons.map((icon) => {
-      let keys = Object.keys(icon);
-      if (keys.includes(btnName)) {
-        stayIcons = icon;
-      }
-    });
-
-    return (
-      <Icon
-        icon={Object.entries(stayIcons)[0][1]}
-        name={Object.entries(stayIcons)[0]}
-        fontSize="16px"
-        className="me-2"
-      />
-    );
-  };
-
-  const stays = ["bed", "apartment", "hotel"];
 
   return (
     <>
